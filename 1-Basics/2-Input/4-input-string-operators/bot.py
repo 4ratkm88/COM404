67233dfe61b62eb28c39a1bd015ffdd32dea40f8
4ratkm88 <4ratkm88@solent.ac.uk>
2019-09-24 11:07:10 +0000

    wef

---
+++ 1-Basics/2-Input/4-input-string-operators/bot.py
@@ -0,0 +1,14 @@
+print("Please enter the number of lives.")
+ 3
+ 
+ print("Please enter the energy level.")
+ 10
+ 
+ print("Please enter the shield level.")
+ 7
+ 
+ print("Health has been set.")
+ 
+ print("Lives:  ♥♥♥")
+ print("Energy: ♦♦♦♦♦♦♦♦♦♦")
+ print("Shield: ♦♦♦♦♦♦♦")
