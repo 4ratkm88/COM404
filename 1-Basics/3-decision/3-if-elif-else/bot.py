b95f08f1f70d5adc924ba961e6cd58abdc1eb1fe
4ratkm88 <4ratkm88@solent.ac.uk>
2019-10-01 08:58:29 +0000

    blabla

---
+++ 1-Basics/3-decision/3-if-elif-else/bot.py
@@ -0,0 +1,9 @@
+print("Towards which direction should I paint (up, down, left or right)?")
+DIR=str(input())
+if (DIR == "up"):
+ print("I am painting upwards direction")
+ elif (DIR == "down"):
+    print("I am painting downward")
+    elif (DIR == "left"):
+        print("Painting left")
+        Else: ( "Painting right")
