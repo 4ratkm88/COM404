28ea9154eefd1d65eb29c8a24008414b57a593c9
4ratkm88 <4ratkm88@solent.ac.uk>
2019-10-08 11:27:00 +0000

    blabla

--- 1-Basics/4-repetitions/2-for-loop/3-range/bot.py
+++ 1-Basics/4-repetitions/2-for-loop/3-range/bot.py
@@ -1,21 +1,23 @@
 import os
 import time
 
-
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
+time.sleep(2)
 print("What level of brightness is required?")
 count=int(input())
-time.sleep(1)
+time.sleep(2)
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
 print("Adjusting brightness.")
-
-time.sleep(1)
-#os.system('cls')
+time.sleep(2)
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
 print("Adjusting brightness..")
-
-time.sleep(1)
-
+time.sleep(2)
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
 print("Adjusting brightness...")
-time.sleep(1)
+time.sleep(2)
+os.system('cls' if os.name == 'nt' else "printf '\033c'")
 for count in range(0, count, 2):
-    time.sleep(1)
     print("Beep's brightness level:"+int(count)*"*")
     print("Bop's brightness level:"+int(count)*"*")
+    time.sleep(1)
+    os.system('cls' if os.name == 'nt' else "printf '\033c'")
