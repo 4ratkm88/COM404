74cf1ccc00c721cc9f6eb6c5c93b08b5e08c438f
4ratkm88 <4ratkm88@solent.ac.uk>
2019-10-08 11:37:18 +0000

    bjks

--- 1-Basics/4-repetitions/2-for-loop/3-range/bot.py
+++ 1-Basics/4-repetitions/2-for-loop/3-range/bot.py
@@ -16,8 +16,10 @@ os.system('cls' if os.name == 'nt' else "printf '\033c'")
 print("Adjusting brightness...")
 time.sleep(2)
 os.system('cls' if os.name == 'nt' else "printf '\033c'")
-    for count in range(0, count, 2):
+for count in range(0, count, 2):
+    time.sleep(1)
     print("Beep's brightness level:"+int(count)*"*")
     print("Bop's brightness level:"+int(count)*"*")
-    time.sleep(1)
+    time.sleep(2)
     os.system('cls' if os.name == 'nt' else "printf '\033c'")
+    
