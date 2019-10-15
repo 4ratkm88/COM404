e6d5f79ddaef14fa51d53a1b99bddd1a2c7a57e3
4ratkm88 <4ratkm88@solent.ac.uk>
2019-10-15 08:48:57 +0000

    rger

---
+++ 1-Basics/5-Function/1-simple/bot.py
@@ -0,0 +1,7 @@
+def listen():
+    print("What sound did I hear?")
+    name = input()
+    print("That was a loud", name)
+    
+
+listen()
---
+++ 1-Basics/5-Function/2-nesting/bot.py
@@ -0,0 +1,11 @@
+def identify():
+    
+    print("What lies before us?")
+    name = input()
+    if name == "A large boulder":
+        print("Time to run!")
+    else:
+        print("Its fine")
+    
+
+identify()
