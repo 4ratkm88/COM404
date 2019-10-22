3e0739900e098c0488089a116be3b39565dde17c
4ratkm88 <4ratkm88@solent.ac.uk>
2019-10-22 11:01:43 +0000

    all

--- 1-Basics/Test/Q7/function.py
+++ 1-Basics/Test/Q7/function.py
@@ -1,3 +1,12 @@
-def under(underword):
-    print(underword)
-    print("*"*len(underword))
+def under(word):
+    print(word)
+    print("*"*len(word))
+
+def over(word):
+    print("*"*len(word))
+    print(word)
+
+def underover(word):
+    print("*"*len(word))
+    print(word)
+    print("*"*len(word))
