e1e2218ca62f9116afa894d909321a73aa9408a2
4ratkm88 <4ratkm88@solent.ac.uk>
2019-11-05 10:56:04 +0000

    all

--- 2-GUIS/1-classes-and-objects/bot.py
+++ 2-GUIS/1-classes-and-objects/bot.py
@@ -1,18 +1,29 @@
+import time
+
 class Bot:
     def __init__(self, name, age, energy, shield, summary):
-        self.display_name = name
-        self.display_age = age
-        self.display_energy = energy
-        self.display_shield = shield
-        
-        
+        self.name = name
+        self.age = age
+        self.energy = energy
+        self.shield = shield
+        self.summary = summary
+
     def display(self):
         print("{} is {} years old".format(self.name, self.age))
         print("{} has {} energy".format(self.name, self.energy))
         print("{} has {} shield".format(self.name, self.shield))
-        print("{} has {} attributes".format(self.name,self.summary))
-        
-charlie = Bot("Charlie", 4545, 85, 5, 6)
+        print("{} has {} attributes".format(self.name, self.summary))
+
+    def age(self):
+        print("{}  {} ".format(self.name, self.age))
+       
+
+
+
+charlie = Bot("Charlie",4545, 5, 5, 5)
+
 
 charlie.display()
-    
+
+
+
