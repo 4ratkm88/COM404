ad8a0d4d7fe8a5f5618e6222872e0072f3ba1a4b
Mark Ratkai <4ratkm88@solent.ac.uk>
2019-11-12 12:13:18 +0000

    all

--- 2-GUIS/2-window-layouts/3-grid/gui.py
+++ 2-GUIS/2-window-layouts/3-grid/gui.py
@@ -22,30 +22,32 @@ class Gui(Tk):
    
     def add_email_frame(self):
         self.email_frame = Frame()
-        self.email_frame.pack()
+        self.email_frame.grid(row=1, column=3, columnspan=1)
 
     def add_heading_label(self):
     # create   
         self.heading_label = Label()
-        self.heading_label.pack()
+        self.heading_label.grid(row=2, column=3, columnspan=5)
         self.heading_label.configure(font="Arial 24",text="Receive our newsletter")
 
     
     # style
     def add_middle_label(self):
         self.middle_label = Label()
-        self.middle_label.pack()
+        self.middle_label.grid(row=3, column=3, columnspan=5)
         self.middle_label.configure(font="Arial 10", text="Please enter your email below to receive our newsletter")
-
-    def add_button(self):
-        self.button = Button()
-        self.button.pack()
-        self.button.configure(font="comic 12", text="Subscribe")
 
     def add_entry(self):
         self.entry = Entry()
-        self.entry.pack()
+        self.entry.grid(row=5, column=3, columnspan=5)
         self.entry.configure(font="comic 12", text="Enter your mail here")
+
+    def add_button(self):
+        self.button = Button()
+        self.button.grid(row=7, column=3, columnspan=5)
+        self.button.configure(font="comic 12", text="Subscribe")
+
+    
 
     #def add_border(self):
         #self.border = Bd()
