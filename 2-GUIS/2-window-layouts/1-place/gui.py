87edaf6401e8e51b5d24a1c97f653b33dd0af589
Mark Ratkai <4ratkm88@solent.ac.uk>
2019-11-12 11:12:32 +0000

    all

--- 2-GUIS/2-window-layouts/1-place/gui.py
+++ 2-GUIS/2-window-layouts/1-place/gui.py
@@ -9,10 +9,14 @@ class Gui(Tk):
     # set window attributes
         self.title("Newsletter")
         self.configure(bg="#eee",
-                   height=500, 
-                   width=500)
+                   height=300, 
+                   width=400)
                    
         self.add_heading_label()
+        self.add_middle_label()
+        self.add_button()
+        self.add_entry()
+        #self.add_border()
 
 
     def add_heading_label(self):
@@ -21,10 +25,25 @@ class Gui(Tk):
         self.heading_label.place(x=0, y=0)
     
     # style
-        self.heading_label.configure(font="Arial 24",
-                                 text="Receive our newsletter")
+        self.heading_label.configure(font="Arial 24",text="Receive our newsletter")
 
     def add_middle_label(self):
         self.middle_label = Label()
-        self.middle_label.place(x=100, y=100)
-        self.middle_label.configure(font="Arial 12", text="Middle Label")
+        self.middle_label.place(x=0, y=100)
+        self.middle_label.configure(font="Arial 10", text="Please enter your email below to receive our newsletter")
+
+    def add_button(self):
+        self.button = Button()
+        self.button.place(x=100, y=200)
+        self.button.configure(font="comic 12", text="Subscribe")
+
+    def add_entry(self):
+        self.entry = Entry()
+        self.entry.place(x=100, y=150)
+        self.entry.configure(font="comic 12", text="Enter your mail here")
+
+    #def add_border(self):
+        #self.border = Bd()
+        #self.border.place(x=280, y=380)
+        #self.border.configure()
+
