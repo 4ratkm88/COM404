e01fee076d16b1770da94b746c7ce1ad3b757a18
Mark Ratkai <4ratkm88@solent.ac.uk>
2019-11-12 10:52:45 +0000

    all

--- 2-GUIS/2-window-layouts/1-place/gui.py
+++ 2-GUIS/2-window-layouts/1-place/gui.py
@@ -3,27 +3,28 @@ from tkinter import *
 class Gui(Tk):
 
   # initialise window
-  def __init__(self):
-    super().__init__()
+    def __init__(self):
+        super().__init__()
 
     # set window attributes
-    self.title("Newsletter")
-    self.configure(bg="#eee",
+        self.title("Newsletter")
+        self.configure(bg="#eee",
                    height=500, 
                    width=500)
                    
-    self.add_heading_label()
+        self.add_heading_label()
 
 
-  def add_heading_label(self):
+    def add_heading_label(self):
     # create   
-    self.heading_label = Label()
-    self.heading_label.place(x=0, y=0)
+        self.heading_label = Label()
+        self.heading_label.place(x=0, y=0)
     
     # style
-    self.heading_label.configure(font="Arial 24",
-                                 text="Receive our newsletter"
-  def add_button(self)
-    #create
-    self.button = Button()
-    self.button.place(x=100, y=100)
+        self.heading_label.configure(font="Arial 24",
+                                 text="Receive our newsletter")
+
+    def add_middle_label(self):
+        self.middle_label = Label()
+        self.middle_label.place(x=100, y=100)
+        self.middle_label.configure(font="Arial 12", text="Middle Label")
